0fda76fd3c67474d73f4a8d16294d0c1794ce859
pomtree <tomrpree@gmail.com>
2019-01-22 16:47:04 -0800

    Modified DJ to include single, single value/no print out

--- src/stat/Dijkstra.java
+++ src/stat/Dijkstra.java
@@ -132,5 +132,62 @@ public class Dijkstra {
         }
     }
 
+    public static int dijkstra(int[][] grid) {
+        int cx = -1, cy = -1;
+        int[][] score = new int[grid.length][grid[0].length];
+        boolean[][] visited = new boolean[grid.length][grid[0].length];
+        for (int a = 0; a < grid.length; a++) {
+            for (int i = 0; i < grid[a].length; i++) {
+                score[a][i] = 500;
+                if (grid[a][i] == -1) {
+                    score[a][i] = 0;
+                    cx = a;
+                    cy = i;
+                }
+                if (grid[a][i] == 1) {
+                    score[a][i] = 501;
+                    visited[a][i] = true;
+                }
+            }
+        }
+        for (int itest = 1; itest < 150; itest++) {
+            int new_cx = -1, new_cy = -1;
+            if (cy + 1 < grid.length && grid[cx][cy + 1] != 1) {
+                score[cx][cy + 1] = Math.min(score[cx][cy + 1], score[cx][cy] + 1);
+                if (!visited[cx][cy + 1]) {
+                    new_cx = cx;
+                    new_cy = cy + 1;
+                }
+            }
+            if (cy - 1 >= 0 && grid[cx][cy - 1] != 1) {
+                score[cx][cy - 1] = Math.min(score[cx][cy - 1], score[cx][cy] + 1);
+                if (!visited[cx][cy - 1]) {
+                    new_cx = cx;
+                    new_cy = cy - 1;
+                }
+            }
+            if (cx - 1 >= 0 && grid[cx - 1][cy] != 1) {
+                score[cx - 1][cy] = Math.min(score[cx - 1][cy], score[cx][cy] + 1);
+                if (!visited[cx - 1][cy]) {
+                    new_cx = cx - 1;
+                    new_cy = cy;
+                }
+            }
+            if (cx + 1 < grid[0].length && grid[cx + 1][cy] != 1) {
+                score[cx + 1][cy] = Math.min(score[cx + 1][cy + 1], score[cx][cy] + 1);
+                if (!visited[cx + 1][cy]) {
+                    new_cx = cx + 1;
+                    new_cy = cy;
+                }
+            }
+            if (grid[cx][cy] == -2)
+                return score[cx][cy];
+            visited[cx][cy] = true;
+            cx = new_cx;
+            cy = new_cy;
+        }
+        return -1;
 
+
+    }
 }
